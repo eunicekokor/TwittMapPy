1e2dd673c072e4c7b55f006a56364f854501476c
Eunice Kokor <eek2138@columbia.edu>
2016-10-27 22:28:18 -0400

    py3

--- app.py
+++ app.py
@@ -1,24 +1,61 @@
+# import json
+# import requests
+# import tweepy
+# import time
+# from elasticsearch import Elasticsearch as es
+
+# from tweepy.streaming import StreamListener
+# from flask import Flask, render_template
+# import certifi
+
+# endpoint = 'tweetmap-env.bmcf8muqyd.us-west-2.elasticbeanstalk.com'
+
+# es = es(host=endpoint,port=443, use_ssl=True, verify_certs=True, ca_certs=certifi.where())
+
+# app=Flask(__name__)
+# @app.route("/")
+# def main():
+#     return render_template('index.html')
+
+
+# @app.route('/<key>')
+# def search(key):
+#     result = es.search(index='tweet',body={"query":{"match": {'text': key}}})
+#     data = json.dumps(result);
+#     return data
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
+from flask import Flask
+from flask import render_template
+from elasticsearch import Elasticsearch
+import certifi
 import json
-import requests
-import tweepy
-import time
-from elasticsearch import Elasticsearch as es
 
-from tweepy.streaming import StreamListener
-from flask import Flask, render_template
+# EB looks for an 'application' callable by default.
+application = Flask(__name__)
 
+endpoint = 'tweetmap-env.bmcf8muqyd.us-west-2.elasticbeanstalk.com'
 
-app=Flask(__name__)
-@app.route("/")
-def main():
+es = Elasticsearch(host=endpoint,port=443, use_ssl=True, verify_certs=True, ca_certs=certifi.where())
+
+@application.route('/')
+def index():
     return render_template('index.html')
 
 
-@app.route('/<key>')
+@application.route('/<keyword>')
 def search(keyword):
-    result = es.search(index='tweet',body={"query":{"match": {'text': key}}})
+    result = es.search(index='twittmap',body={"query":{"match": {'text': keyword}}})
     data = json.dumps(result);
     return data
 
+
+# run the app.
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run(port=8081)
--- server.py
+++ server.py
@@ -1,63 +1,113 @@
-import tweepy
-from tweepy.streaming import StreamListener
+# import tweepy
+# from tweepy.streaming import StreamListener
+# import json
+# from elasticsearch import Elasticsearch
+# import time
+# from tweepy import OAuthHandler
+# from tweepy import Stream, API
+# from flask import Flask, render_template
+# from TwitterAPI import TwitterAPI
+# import googlemaps
+
+# consumer_key= "euXCzLT4bHep6PMSwFha1X610"
+# consumer_secret= "czLjLODWigoHvUxdXR7KhPoucrTP36HVxZtK19wqDATpQjM3tW"
+# access_token= "28203065-m0YfUbocnLSgTzmfV5kYX7FIhLHo71s9Pb36yu3jB"
+# access_token_secret= "1xc2XNwgXhCEm34NbhDeuIEnPuDAqHkUrG2Wpp7W1p2ge"
+# es = Elasticsearch([{'host': 'http://tweetmap-env.bmcf8muqyd.us-west-2.elasticbeanstalk.com/', 'port': 443}])
+
+# gmaps = googlemaps.Client(key='AIzaSyACGeO9TeMbOLSk1We5O5Yrh5FXnGUQSbs')
+
+
+# class TweetPyListener(StreamListener):
+#     def on_data(self, data):
+#             tweet = json.loads(data)
+#             if tweet['lang'] == 'en' and tweet['user'].get('location') is not None:
+#               location = tweet['user'].get('location')
+#               tweet_id = str(tweet['id'])
+#               geocode_result = gmaps.geocode(location)
+#               raw_tweet = {
+#                   'user': tweet['user']['screen_name'],
+#                   'text': tweet['text'],
+#                   'location': tweet['user']['location'],
+#                   'coordinates': geocode_result,
+#                   'time': tweet['created_at']
+#               }
+#               es.index(index='tweets', id=tweet_id, body=raw_tweet)
+
+
+#     def on_error(self, status):
+#         print(status)
+#         if str(status) == '420':
+#             print "Sleeping 3 sec"
+#             time.sleep(30)
+#         return
+
+# if __name__ == "__main__":
+#     auth = OAuthHandler(consumer_key, consumer_secret)
+#     auth.set_access_token(access_token, access_token_secret)
+
+#     es = Elasticsearch(hosts=['http://tweetmap-env.bmcf8muqyd.us-west-2.elasticbeanstalk.com/'])
+#     # es.indices.create(index='twitter',ignore = 400)
+#     l = TweetPyListener()
+#     stream = Stream(auth, l)
+
+#             # stream.filter(track=['haiku', 'poem', 'poetry'])
+
+#     while True:
+#        stream.filter(track=['sports', 'music', 'hillary'])
+
 import json
-from elasticsearch import Elasticsearch
-import time
+from elasticsearch import Elasticsearch as es
+import tweepy
+import pprint
+from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
-from flask import Flask, render_template
+import sys
+import datetime
+import time
 from TwitterAPI import TwitterAPI
 
+access_token="28203065-m0YfUbocnLSgTzmfV5kYX7FIhLHo71s9Pb36yu3jB"
+access_token_secret="1xc2XNwgXhCEm34NbhDeuIEnPuDAqHkUrG2Wpp7W1p2ge"
+consumer_key="euXCzLT4bHep6PMSwFha1X610"
+consumer_secret="czLjLODWigoHvUxdXR7KhPoucrTP36HVxZtK19wqDATpQjM3tW"
 
-consumer_key= "euXCzLT4bHep6PMSwFha1X610"
-consumer_secret= "czLjLODWigoHvUxdXR7KhPoucrTP36HVxZtK19wqDATpQjM3tW"
-access_token= "28203065-m0YfUbocnLSgTzmfV5kYX7FIhLHo71s9Pb36yu3jB"
-access_token_secret= "1xc2XNwgXhCEm34NbhDeuIEnPuDAqHkUrG2Wpp7W1p2ge"
-api = TwitterAPI(consumer_key, consumer_secret, access_token, access_token_secret)
+class StdOutListener(StreamListener):
 
-class TweetPyListener(StreamListener):
-    def on_data(self, data):
-        if data:
-            # print data
-            jsonData = json.loads(data)
-            if jsonData['place'] is not None:
-                es.index(index='twitter',doc_type='tweet',body=jsonData)
-                print jsonData
-                return True
-            if jsonData['coordinates'] is not None:
-                es.index(index='twitter',doc_type='tweet',body=jsonData)
-                print jsonData
-                return True
+  def on_data(self, doc_data):
+      print (doc_data)
+      json_data=json.loads(doc_data)
+      try:
+          data={}
+          if json_data['coordinates'] is not None:
+              data["lat"]=json_data['coordinates']['coordinates'][0]
+              data["longtitude"]=longtitude=json_data['coordinates']['coordinates'][1]
+          elif json_data['place'] is not None:
+              data["lat"]=json_data['place']['bounding_box']['coordinates'][0][0][0]
+              data["longtitude"]=json_data['place']['bounding_box']['coordinates'][0][0][1]
+          else:
+              return
+          data["text"]=json_data['text']
+          data["name"]=json_data['user']['name']
+          data["created_at"]=json_data['created_at']
+          print data
+          res=es.index(index="tweet", doc_type='tweet', body=data)
+          print (res['created'])
+      except:
+          return
 
-
-
-    def on_error(self, status):
-        print(status)
-        if status == 420:
-            print "Sleeping 3 sec"
-            time.sleep(30)
-        return
-
-    def on_exception(self, exception):
-        """Called when an unhandled exception occurs."""
-        time.sleep(30)
-        return
-
+  def on_error(self, status):
+      print status
 
 if __name__ == "__main__":
-    es = Elasticsearch(hosts=['http://tweetmap-env.bmcf8muqyd.us-west-2.elasticbeanstalk.com/'])
-    # es.indices.create(index='twitter',ignore = 400)
-    l = TweetPyListener()
-
-    # auth = OAuthHandler(consumer_key, consumer_secret)
-    # auth.set_access_token(access_token, access_token_secret)
-    print "WHY"
     while True:
-      try:
-        # stream = Stream(auth, l)
-        api.filter(track=['election', 'polls', '538','government', 'us', 'america', 'vote', 'ballot', 'trump', 'clinton'])
-        # r = api.request('statuses/filter', {'track':'election,polls,538,government,us,america,vote,ballot,trump,clinton'})
-        # for item in r:
-        # print(item)
-      except Exception as e:
-        pass
+        try:
+            l = StdOutListener()
+            auth = OAuthHandler(consumer_key, consumer_secret)
+            auth.set_access_token(access_token, access_token_secret)
+            stream = Stream(auth, l)
+            stream.filter(track=['haiku', 'poem', 'poetry', 'obama', 'clinton'])
+
+        except:
+            continue
